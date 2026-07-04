f44b2542ad3aaf56dda57ca90821d91860a89d6e
jay-waves <yujiawei@buaa.edu.cn>
2026-07-04 11:30:03 +0800

    Simplify personal site

--- src/components/binary-divider.tsx
+++
@@ -1,9 +0,0 @@
-const BINARY_STREAM = '01'.repeat(240)
-
-export function BinaryDivider() {
-  return (
-    <p className="binary-divider" aria-hidden="true">
-      <span>{BINARY_STREAM}</span>
-    </p>
-  )
-}
